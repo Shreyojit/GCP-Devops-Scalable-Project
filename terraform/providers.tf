91576364eca3306011ed6db89a98d79306408670
jeet <shreyojitdas95@gmail.com>
2025-01-26 01:09:07 +0530

    change-prod

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,6 +1,8 @@
 terraform {
   required_version = ">= 0.12"
   backend "gcs" {
+    bucket = "my-devops-tf-state-bucket"  # Replace with your GCS bucket name
+    prefix = "terraform/state"            # Optional: Prefix for the state file
   }
 }
 provider "google" {
@@ -8,12 +10,14 @@ provider "google" {
   region  = var.region
 }
 provider "kubernetes" {
-  host  = google_container_cluster.default.endpoint
+  host  = "https://${google_container_cluster.default.endpoint}"  # Ensure it starts with https://
   token = data.google_client_config.current.access_token
   client_certificate = base64decode(
     google_container_cluster.default.master_auth[0].client_certificate,
   )
-  client_key = base64decode(google_container_cluster.default.master_auth[0].client_key)
+  client_key = base64decode(
+    google_container_cluster.default.master_auth[0].client_key,
+  )
   cluster_ca_certificate = base64decode(
     google_container_cluster.default.master_auth[0].cluster_ca_certificate,
   )
